ac8466d7a6742b67bb4a16e430039ff74f11f9fa
Lukáš Horák <lukas.horak.05@gmail.com>
2022-02-08 09:07:11 +0100

    init day3

---
+++ LukasHorak/Day2/Json/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ LukasHorak/Day3/Day3 - the dayining/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day3___the_dayining
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
