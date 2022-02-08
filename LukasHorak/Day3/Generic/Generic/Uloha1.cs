4cf5faa2b8b3f9660f595185c563913aea253190
Lukáš Horák <lukas.horak.05@gmail.com>
2022-02-08 11:43:19 +0100

    generic uloha1

---
+++ LukasHorak/Day3/Generic/Generic/Uloha1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UlohyUtility;
+
+namespace Generic.Generic
+{
+    interface INotificable
+    {
+        void Notify(string text);
+    }
+
+    class Evidence<_T> : List<_T> where _T : INotificable
+    {
+        public void NotifyAll(string text)
+        {
+            foreach (var item in this)
+            {
+                item.Notify(text);
+            }
+        }
+    }
+
+    class Employee : INotificable
+    {
+        public string Name { get; }
+
+        public Employee(string name)
+        {
+            Name = name;
+        }
+
+        public void Notify(string text)
+        {
+            Console.WriteLine($"{Name}: {text}");
+        }
+    }
+
+    class Uloha1 : IUloha
+    {
+        public void Execute()
+        {
+            var ev = new Evidence<Employee>()
+            {
+                new Employee("Bob"),
+                new Employee("Jill"),
+                new Employee("Jake"),
+                new Employee("John"),
+                new Employee("Bill"),
+                new Employee("Gill"),
+            };
+
+            ev.NotifyAll("vstavame");
+        }
+    }
+}
---
+++ LukasHorak/Day3/Generic/Program.cs
@@ -0,0 +1,13 @@
+﻿using Generic.Generic;
+using System;
+
+namespace Generic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Uloha1().Execute();
+        }
+    }
+}
